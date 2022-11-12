f8f3163e74fc79e803576fa8a9bf3ddfa8e60f0e
WaRaN1 <bezckorovayniy1988@gmail.com>
2022-11-12 16:45:51 +0200

    first commit

--- Module_12_Cтруктури данних/DZ_11_11_list_of_numbers.py
+++ Module_12_Cтруктури данних/DZ_11_11_list_of_numbers.py
@@ -3,9 +3,5 @@
 def num(numbers):
     return [i for i in range(1, numbers + 1)]
 
-# num = num(5)
-# for i in num:
-#     print(i)
-
 if __name__ == "__main__":
-    print(num(5))
+    print(num(11))
